133cc32a2720b0a088385e24010ba2b44d407e56
ErinHandayaniA <98836908+ErinHandayaniA@users.noreply.github.com>
2022-02-06 18:47:35 +0700

    penjelasan source code
    1.	Fungsi #include adalah sebuah prosesor pengarah yang mengatakan kepada kompiler untuk meletakan kode dari header file iostream.h kedalam program. 
    2.	iostream adalah yang ktai sebut file-header yang mana yang akan kita bahas dalam artikel ini. Yang mana iostream adalah file header yang merupakan kepanjangan dari Input Output Stream yang digunakan untuk memanggil fungsi yang ada pada library file-header tersebut seperti fungsi input yaitu cin dan fungsi output yaitu  cout. 
    3.	using namespace std adalah sebuah instruksi terhadap kompiler untuk menggunakan semua fungsi yang terkait untuk kerangka yang sama, bisa berupa berkas/file , class, dan sejenisnya yang berkaitan dengan std. 
    4.	Maksud dari int di depan main adalah jenis tipe data yang akan dikembalikan nilainya kembali. Di dalam fungsi main(), wajib kamu sertakan return 0. Artinya, fungsi main akan mengembalikan nilai 0 setelah dieksekusi. 
    5.	Fungsi main merupakan fungsi utama sebuah program. Fungsi ini juga akan dieksekusi untuk pertama kalinya. Oleh karena itu, kamu harus selalu menyertakan fungsi main() dalam sebuah program
    6.	Fungsi Tanda () digunakan untuk mengapit argumen fungsi yaitu nilai yang akan dilewatkan ke fungsi.
    7.	Fungsi Tanda { pada fungsi main() menyatakan awal eksekusi program. Adapun } pada fungsi main() menyatakan akhir eksekusi program.
    8.	Tipe double memiliki tingkat ketelitian secara ganda atau double precision dengan menggunakan ruang penyimpanan 64-bit dalam menyimpan nilainya. Tipe tersebut pastinya memberikan kemampuan menghitung matematis secara lebih cepat dari tipe float.
    9.	Tanda L, a , t adalah sebagai variable dalam pemrograman ini.   
    10.	Fungsi Tanda semicolon “ ; ” digunakan untuk mengakhiri sebuah pernyataan. Setiap pernyataan harus diakhiri dengan sebuah tanda semicolon.  
    11.	Fungsi cout (character out) dipakai untuk menampilkan text di layar monitor anda. Cout merupakan sebuah objek di dalam C++, yang digunakan untuk mengarahkan data ke dalam standar output (cetak pada layar) 
    12.	Fungsi Tanda <<, yang diketahui sebagai operator pemasukan (insertion operators). Tanda tersebut mengatakan kepada kompiler agar segera menghasilkan output sesuai dengan input anda.
    13.	tanda kutip ganda membuat sebuah string literal.  
    14.	\n adalah simbol untuk membuat baris baru. 
    15.	endl merupakan suatu fungsi manipulator yang digunakan untuk menyisipkan karakter NewLine atau mengatur pindah baris. Fungsi ini sangat berguna untuk piranti keluaran berupa file di disk. 
    16.	\t adalah simbol untuk horizontal tab ( teks akan bergeser ke kanan sebagai pengganti tab)
    17.	Fungsi cin (c input) adalah fungsi untuk mengambil input dari keyboard. Fungsi cin membutuhkan variabel untuk menyimpan data yang diinputkan.
    18.	Fungsi Tanda >>, yang diketahui sebagai operator pemasukan (insertion operators). Tanda tersebut mengatakan kepada kompiler agar segera menghasilkan input sesuai dengan output anda. 
    19.	Tanda * berfungsi sebagai perkalian.

---
+++ Jajargenjang_ErinHandayaniA.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+double L, a, t;
+cout<<"Menghitung Luas Jajargenjang\n==============================="<<endl;
+cout<<"masukkan nilai alas \t = ";
+cin>>a;
+cout<<"masukkan nilai tinggi \t = ";
+cin>>t;
+
+L = (a * t);
+
+cout<<"luas jajargenjang yang diketahui : "<<endl;
+cout<<"alas \t = "<<a << " cm" <<endl;
+cout<<"tinggi \t = "<<t << " cm" <<endl;
+cout<<"adalah : "<<endl;
+cout<<"L = (a x t)\nL = ("<<a<<" x "<<t<<")"<<endl;
+cout<<"L = "<<L<<" cm"<<endl;
+cout<<"Luas Jajargenjang tersebut adalah = "<<L<<" cm"<<endl;
+
+
+}
